0c6eea1a6ba492ed84850b91d4ade1e6d4cd64e6
alstonjohnson <johnson.alston@icloud.com>
2024-11-06 19:47:25 -0600

    updated index.js

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// Basic GraphQL setup
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, MERN Stack!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}${server.graphqlPath}`);
+});
