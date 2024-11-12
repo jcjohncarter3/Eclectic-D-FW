f8a0988c5e75f99864f63d0a3893b6c1b68d1bbb
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-11-11 21:02:47 -0600

    added Venue model

---
+++ server/models/Venue.js
@@ -0,0 +1,20 @@
+const { Schema } = require("mongoose");
+
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+const venueSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+});
+
+module.exports = venueSchema;
