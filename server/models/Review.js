4db6cd37028356c4230f6455c1b25cc5cb1ce888
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-11-11 21:35:06 -0600

    updated models and typeDefs

--- server/models/Review.js
+++ server/models/Review.js
@@ -2,10 +2,10 @@ const { Schema } = require("mongoose");
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const reviewSchema = new Schema({
-  title: {
+  text: {
     type: String,
   },
-  body: {
+  username: {
     type: String,
     required: true,
   },
--- server/models/Venue.js
+++ server/models/Venue.js
@@ -2,19 +2,17 @@ const { Schema } = require("mongoose");
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const venueSchema = new Schema({
-  title: {
+  name: {
     type: String,
     required: true,
+  },
+  location: {
+    type: String,
   },
   description: {
     type: String,
   },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
+  reviews: [reviewSchema],
 });
 
 module.exports = venueSchema;
