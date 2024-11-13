596ba9ab28bbd3447eaf6bbc9a95a312335e3af6
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-11-12 18:16:07 -0600

    integrated the venues query

--- client/src/pages/Venues.jsx
+++ client/src/pages/Venues.jsx
@@ -1,11 +1,23 @@
-
+import { useQuery } from '@apollo/client'
+import { QUERY_ALL_VENUES } from '../graphql/queries'
 // This is the page that lists all the venues
 export default function Venues() {
+    const { loading, data } = useQuery(QUERY_ALL_VENUES)
+
+    if (loading) return <div>Loading all venues...</div>
+    console.log("datassss: ", data)
+    const venues = data.venues;
     return (
         <div className="page">
             <h1>All Venues</h1>
             <div>
-                Lists all the venues here xoxo
+                {venues.map((venue) => {
+                    return <div key={venue._id} style={{ border: "1px solid red"}}>
+                        <p>{venue.name}</p>
+                        <p>{venue.location}</p>
+                        <p>{venue.description}</p>
+                    </div>
+                })}
             </div>
         </div>
     )
