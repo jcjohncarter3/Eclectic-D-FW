6da26b8dd0c1f975587c24b24dc455128ca89a96
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-11-13 14:52:20 -0600

    Added Basic Header.

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,15 +1,30 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav>
       <ul>
-        <li><NavLink to="/" activeClassName="active">Explore</NavLink></li>
-        <li><NavLink to="/live-music" activeClassName="active">Live Music</NavLink></li>
-        <li><NavLink to="/music-venue" activeClassName="active">Music Venue</NavLink></li>
-        <li><NavLink to="/reviews" activeClassName="active">Reviews</NavLink></li>
+        <li>
+          <NavLink to="/" activeClassName="active">
+            Explore
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/live-music" activeClassName="active">
+            Live Music
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/music-venue" activeClassName="active">
+            Music Venue
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/reviews" activeClassName="active">
+            Reviews
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
