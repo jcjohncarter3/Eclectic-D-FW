de40574b9c622cbf26103a8a72ca3bc1bbbd1f26
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-11-13 14:29:12 -0600

    Integrated Reviews.

--- client/src/pages/MusicVenuePage.jsx
+++ client/src/pages/MusicVenuePage.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import {
   QUERY_ALL_VENUES,
   QUERY_ALL_REVIEWS_PER_VENUE,
 } from "../graphql/queries";
-// import { ADD_REVIEW } from '../graphql/queries';
+import { ADD_REVIEW } from "../graphql/mutations";
 import { useParams } from "react-router-dom";
 
 function MusicVenuePage() {
@@ -22,22 +22,40 @@ function MusicVenuePage() {
   } = useQuery(QUERY_ALL_REVIEWS_PER_VENUE, {
     variables: { venueId },
   });
-  // const [addReview] = useMutation(ADD_REVIEW);
+  const [addReview, { error }] = useMutation(ADD_REVIEW);
   const [reviewText, setReviewText] = useState("");
+  const [reviewRating, setReviewRating] = useState(1);
+  const [reviewList, setReviewList] = useState([]);
+
+  useEffect(() => {
+    const reviews = dataReviews?.reviewsByVenue || [];
+
+    setReviewList(reviews);
+  }, [dataReviews]);
+
+  // useEffect(() => {}, [dataReviews]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // try {
-    //   await addReview({
-    //     variables: {
-    //       venueId,
-    //       reviewText,
-    //     },
-    //   });
-    //   setReviewText('');
-    // } catch (error) {
-    //   console.error('Error posting review', error);
-    // }
+    console.log("reviewText", reviewText);
+    console.log("reviewRating", reviewRating);
+    try {
+      const newReview = await addReview({
+        variables: {
+          text: reviewText,
+          rating: parseInt(reviewRating),
+          venueId,
+          userId: "6734fd0a49dd465ec3288076", // replace with JWT token later
+        },
+      });
+      console.log("new review", newReview);
+      // setReviewList([...reviewList, newReview]);
+      setReviewList((oldArray) => [newReview.data.addReview, ...oldArray]);
+      setReviewText("");
+      setReviewRating(1);
+    } catch (error) {
+      console.error("Error posting review", error);
+    }
   };
 
   if (loadingVenues || loadingReviews) return <p>Loading...</p>;
@@ -45,7 +63,6 @@ function MusicVenuePage() {
   if (errorReviews) return <p>Error: {errorReviews.message}</p>;
   console.log("dataReviews", dataReviews);
   const venue = dataVenues.venues.find((venue) => venue._id === venueId);
-  const reviews = dataReviews.reviewsByVenue || [];
 
   return (
     <div>
@@ -63,16 +80,25 @@ function MusicVenuePage() {
               placeholder="Write your review"
               required
             />
+            <select onChange={(e) => setReviewRating(e.target.value)}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
             <button type="submit">Submit Review</button>
           </form>
         </div>
         <div className="review-list">
-          {reviews.length > 0 &&
-            reviews &&
-            reviews.map((review) => {
+          {reviewList.length > 0 &&
+            reviewList &&
+            reviewList.map((review) => {
               return (
-                <div key={review._id}>
-                  <p>{review.text}</p>
+                <div key={review._id} style={{ border: "1px solid black" }}>
+                  <p>Rating: {review.rating}/5</p>
+                  <p> REview: {review.text}</p>
+                  <p>By: {review.user.username}</p>
                 </div>
               );
             })}
